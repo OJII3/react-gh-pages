c12d7df516b99db7800c968fa09710014085d475
OJII3 <kawaisounayuriko@gmail.com>
2022-10-30 11:17:06 +0900

    Update: improve home.tsx

--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -1,21 +1,26 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Stack, Row } from "react-bootstrap";
 import NavbarElement from "./navbarElement";
 
 const Home = () => {
   return (
     <>
       <NavbarElement />
-
-      <div className="home">Welcome Home</div>
-      <Container>
-        <a href='https://github.com/ojii3'>
-          <img alt='' src='https://avatars.githubusercontent.com/u/84656786?v=4' style={{ borderRadius: '50%' }}></img>
-        </a>
-        <a href="https://github.com/anuraghazra/github-readme-stats">
-          <img alt='' src="https://github-readme-stats.vercel.app/api/top-langs/?username=OJII3&layout=compact" />
-        </a>
-      </Container>
+      <Stack gap={3} className='text-center'>
+        <Row className='text-center'>
+          <h1>Welcome to OJII3's Home!</h1>
+        </Row>
+        <Row className='text-center'>
+          <a href='https://github.com/ojii3'>
+            <img alt='' width='130' height='130' style={{ borderRadius: '50%' }} src='https://avatars.githubusercontent.com/u/84656786?v=4' />
+          </a>
+        </Row>
+        <Row className='text-center'>
+          <a href="https://github.com/anuraghazra/github-readme-stats">
+            <img alt='' src="https://github-readme-stats.vercel.app/api/top-langs/?username=OJII3&layout=compact" />
+          </a>
+        </Row>
+      </Stack>
     </>
   );
 }
